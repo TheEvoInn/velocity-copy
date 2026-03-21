12fffd21001522c53f8645c5dd304911a299a464
Evo Lutions <theevo.inn@gmail.com>
2026-03-21 17:32:48 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,7 @@ import WorkflowBuilder from './pages/WorkflowBuilder';
 import WebhookConfiguration from './pages/WebhookConfiguration';
 import WorkflowArchitect from './pages/WorkflowArchitect';
 import CentralEventLog from './pages/CentralEventLog';
+import VelocitySystemDashboard from './pages/VelocitySystemDashboard';
 
 const AuthenticatedApp = () => {
   const { isLoadingAuth, isLoadingPublicSettings, authError, navigateToLogin } = useAuth();

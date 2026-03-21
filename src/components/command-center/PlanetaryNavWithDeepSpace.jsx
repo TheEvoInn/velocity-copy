ba1a053bdd0d96fbd30b3d44d3add8270ee2ebf2
Evo Lutions <theevo.inn@gmail.com>
2026-03-21 18:59:00 +0000

    File changes

--- src/components/command-center/PlanetaryNavWithDeepSpace.jsx
+++ src/components/command-center/PlanetaryNavWithDeepSpace.jsx
@@ -96,7 +96,8 @@ export default function PlanetaryNavWithDeepSpace({ stats = {} }) {
               </div>
             </div>
           </Link>
-            ))}
-            </div>
-            );
+          ))}
+          </div>
+          );
+          }
             }

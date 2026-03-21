78832b4248b20dc80ad8bdc903f9159e1baf30c4
Evo Lutions <theevo.inn@gmail.com>
2026-03-21 01:46:43 +0000

    File changes

--- src/components/admin/AdminKYCReview.jsx
+++ src/components/admin/AdminKYCReview.jsx
@@ -252,7 +252,11 @@ export default function AdminKYCReview() {
 
   const { data: kycs = [], isLoading, error, refetch } = useQuery({
     queryKey: ['admin_kyc_list'],
-    queryFn: () => base44.entities.KYCVerification.list('-created_date', 500),
+    queryFn: async () => {
+      const res = await base44.functions.invoke('kycAdminService', { action: 'list_all' });
+      if (res.data?.error) throw new Error(res.data.error);
+      return res.data?.records || [];
+    },
     refetchInterval: 20000,
   });
 

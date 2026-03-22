f21671c788ceaa0f4495837668485f34c8c8865a
Evo Lutions <theevo.inn@gmail.com>
2026-03-22 23:23:58 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ const AuthenticatedApp = () => {
   // Render the main app
   return (
     <Routes>
+      <Route path="/AdminControlPanel" element={<AdminControlPanel />} />
       {/* Add your page Route elements here */}
       <Route path="*" element={<PageNotFound />} />
     </Routes>

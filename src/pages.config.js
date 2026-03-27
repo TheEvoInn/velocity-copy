b4b2d44f3008aaa60fe4c0fdb208dd6b1d356edf
TheEvoInn <theevo.inn@gmail.com>
2026-03-26 21:52:53 -0700

    Refactor pages configuration and update imports
     Fix all broken imports and register missing pages

--- src/pages.config.js
+++ src/pages.config.js
@@ -51,11 +51,11 @@
 import Dashboard from './pages/Dashboard';
 import StarshipBridge from './pages/StarshipBridge';
 import Control from './pages/Control';
-import AutoPilot from './pages/AutoPilot';
-import AutoPilotEngine from './pages/AutoPilotEngine';
+import VeloAutopilotControl from './pages/VeloAutopilotControl';
+import UnifiedAutopilot from './pages/UnifiedAutopilot';
 import Discovery from './pages/Discovery';
 import WorkDiscovery from './pages/WorkDiscovery';
-import Execution from './pages/Execution';
+import VeloExecutionEngine from './pages/VeloExecutionEngine';
 import Finance from './pages/Finance';
 import WalletDashboard from './pages/WalletDashboard';
 import CryptoAutomation from './pages/CryptoAutomation';
@@ -63,34 +63,48 @@ import DigitalResellers from './pages/DigitalResellers';
 import NED from './pages/NED';
 import VIPZ from './pages/VIPZ';
 import IdentityManager from './pages/IdentityManager';
-import AdminControlPanel from './pages/AdminControlPanel';
+import AdminPanel from './pages/AdminPanel';
 import Chat from './pages/Chat';
 import SystemAuditDashboard from './pages/SystemAuditDashboard';
+import VeloIdentityHub from './pages/VeloIdentityHub';
+import VeloFinanceCommand from './pages/VeloFinanceCommand';
+import PendingInterventions from './pages/PendingInterventions';
+import Onboarding from './pages/Onboarding';
 import __Layout from './Layout.jsx';
 
 export const PAGES = {
-	"Dashboard": Dashboard,
-	"StarshipBridge": StarshipBridge,
-	"Control": Control,
-	"AutoPilot": AutoPilot,
-	"AutoPilotEngine": AutoPilotEngine,
-	"Discovery": Discovery,
-	"WorkDiscovery": WorkDiscovery,
-	"Execution": Execution,
-	"Finance": Finance,
-	"WalletDashboard": WalletDashboard,
-	"CryptoAutomation": CryptoAutomation,
-	"DigitalResellers": DigitalResellers,
-	"NED": NED,
-	"VIPZ": VIPZ,
-	"IdentityManager": IdentityManager,
-	"AdminControlPanel": AdminControlPanel,
-	"Chat": Chat,
-	"SystemAuditDashboard": SystemAuditDashboard,
+    "Dashboard": Dashboard,
+    "StarshipBridge": StarshipBridge,
+    "Control": Control,
+    "AutoPilot": VeloAutopilotControl,
+    "AutoPilotEngine": UnifiedAutopilot,
+    "Discovery": Discovery,
+    "WorkDiscovery": WorkDiscovery,
+    "Execution": VeloExecutionEngine,
+    "Finance": Finance,
+    "WalletDashboard": WalletDashboard,
+    "CryptoAutomation": CryptoAutomation,
+    "DigitalResellers": DigitalResellers,
+    "NED": NED,
+    "VIPZ": VIPZ,
+    "IdentityManager": IdentityManager,
+    "AdminControlPanel": AdminPanel,
+    "Chat": Chat,
+    "SystemAuditDashboard": SystemAuditDashboard,
+    "VeloAutopilotControl": VeloAutopilotControl,
+    "VeloIdentityHub": VeloIdentityHub,
+    "VeloFinanceCommand": VeloFinanceCommand,
+    "VeloExecutionEngine": VeloExecutionEngine,
+    "PendingInterventions": PendingInterventions,
+    "Onboarding": Onboarding,
+    "AdminPanel": AdminPanel,
+    "UnifiedAutopilot": UnifiedAutopilot,
 }
 
 export const pagesConfig = {
- mainPage: "Dashboard",
- Pages: PAGES,
- Layout: __Layout,
+    mainPage: "Dashboard",
+    Pages: PAGES,
+    Layout: __Layout,
 }
+
+
